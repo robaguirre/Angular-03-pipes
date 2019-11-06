79e196a69293208ec98184fa952e57f599cd9ea6
Roberto Agirre <r.agirre@ibermatica.com>
2019-11-06 17:48:43 +0100

    Varios pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre = 'Roberto';
+
+  lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  numero = 33.9;
+
+  PI = Math.PI;
+
+  porcion = 0.234;
+
+  moneda = 1203.45;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 import { AppComponent } from './app.component';
+
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
